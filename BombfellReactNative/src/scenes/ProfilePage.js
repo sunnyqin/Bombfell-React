f9a8976dd9eb80308216ba5ab75a400728d80058
sunnyqin <sunny@interactivelabs.co>
2016-03-25 13:55:15 +0800

    profile page with static data

--- BombfellReactNative/src/scenes/ProfilePage.js
+++ BombfellReactNative/src/scenes/ProfilePage.js
@@ -11,17 +11,42 @@ var {
     ActivityIndicatorIOS,
     AlertIOS,
     Image,
+    Dimensions,
+    Component,
 } = React;
 
 var API_URL = 'http://demo9383702.mockable.io/users';
+var {width, height} = Dimensions.get('window');
 
-var HeaderView = React.createClass({
-	render: function() {
+class MeasurementLabel extends Component {
+	render() {
 		return (
-			<Image style={styles.headerImage} source={require('../images/profilebg.png')} />
+			<View style={{flexDirection: 'column', flex: 1}}>
+				<Text style={styles.measurementLabel}>{this.props.label}</Text>
+				<Text style={styles.measurementValue}>{this.props.value}</Text>
+			</View>
 		);
 	}
-});
+}
+
+class ProfileHeader extends Component {
+	render() {
+		return (
+			<View style={{flex: 1, flexDirection: 'column'}}>
+				<Image resizeMode="stretch" style={styles.avatar} source={require('../images/avatar.png')}/>
+				<Text style={styles.titleText}>Daniel Lane</Text>
+				<Text style={styles.subtitleText}>daniel@interactivelabs.co</Text>
+				<View style={styles.measurementView}>
+					<MeasurementLabel label="HEIGHT" value="5"/>
+					<MeasurementLabel label="WEIGHT" value="205lbs"/>
+					<MeasurementLabel label="SHOE" value="9.5"/>
+					<MeasurementLabel label="SHIRT" value="XL"/>
+					<MeasurementLabel label="PANT" value="32"/>
+				</View>
+			</View>
+		);
+	}
+}
 
 class ProfilePage extends React.Component {
     constructor(props) {
@@ -66,14 +91,16 @@ class ProfilePage extends React.Component {
 
     renderListView() {
         return (
-        	<View>
-            <View style={styles.headerView}>
-        		<HeaderView></HeaderView>
-        	</View>
+        	<View style={{flexDirection: 'column', alignItems: 'stretch', paddingTop: 64}}>
+        	<Image resizeMode="stretch" style={styles.headerImage} source={require('../images/profilebg.png')}>
+        		<ProfileHeader>
+        		</ProfileHeader>
+        	</Image>
             <View>
                 <ListView
+                	automaticallyAdjustContentInsets={false}
                     dataSource = {this.state.dataSource}
-                    style      = {styles.listview}
+                    style = {styles.listView}
                     renderRow  = {this.renderRow}
                     renderSectionHeader = {this.renderSectionHeader}
                 />
@@ -126,27 +153,68 @@ var styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    headerView: {
-    	flexDirection: 'row',
-    	justifyContent: 'center',
-    	marginTop: 64,
-    	width: 375,
-    	height: 150,
-    },
     headerImage: {
-    	alignSelf: 'stretch',
-    	width: 375,
-    	height: 150
+    	width: width,
+    	height: 250,
     },
     headerText: {
         fontWeight: 'bold',
         fontSize: 10,
         color: '#8A8A8A'
     },
+    listView: {
+    	backgroundColor: '#FFFFFF',
+    },
     text: {
         color: 'white',
         paddingHorizontal: 8,
-        fontSize: 16
+        fontSize: 16,
+    },
+    avatar: {
+    	marginTop: 28,
+    	width: 100,
+    	height: 100,
+    	alignSelf: 'center'
+    },
+    titleText: {
+    	marginTop: 10,
+    	color: 'white',
+    	paddingVertical: 5,
+    	fontSize: 20,
+    	textAlign: 'center',
+    	backgroundColor: 'rgba(0,0,0,0)',
+    },
+    subtitleText: {
+    	color: 'white',
+    	paddingVertical: 1,
+    	fontSize: 12,
+    	fontWeight: '100',
+    	textAlign: 'center',
+    	backgroundColor: 'rgba(0,0,0,0)',
+    },
+    measurementLabel: {
+    	marginTop: 15,
+    	color: 'white',
+    	paddingVertical: 0,
+    	fontSize: 12,
+    	fontWeight: '100',
+    	textAlign: 'center',
+    	backgroundColor: 'rgba(0,0,0,0)',
+    },
+    measurementValue: {
+    	marginTop: 0,
+    	color: 'white',
+    	paddingVertical: 2,
+    	fontSize: 15,
+    	fontWeight: '500',
+    	textAlign: 'center',
+    	backgroundColor: 'rgba(0,0,0,0)',
+    },
+    measurementView: {
+    	flex: 1,
+    	flexDirection: 'row',
+    	justifyContent: 'space-around',
+    	backgroundColor: 'rgba(0,0,0,0)',
     },
     rowStyle: {
         paddingVertical: 20,
